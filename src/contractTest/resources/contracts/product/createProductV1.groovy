7e546873533d6a760e7a492e7cd380d0e2ee107e
Antonio Pires <tonan@outlook.com.br>
2026-01-14 08:02:39 -0300

    Dynamic, validated contracts and stubs

--- src/contractTest/resources/contracts/product/createProductV1.groovy
+++ src/contractTest/resources/contracts/product/createProductV1.groovy
@@ -12,13 +12,34 @@ Contract.make {
         }
         urlPath("/api/v1/products") {
             body([
-                    name       : "Notebook X11",
-                    brand      : "Deep Driver",
-                    regularPrice      : 1500.00,
-                    salePrice      : 1500.00,
-                    enabled: true,
-                    categoryId: "f5ab7a1e-37da-41e1-892b-a1d38275c2f2",
-                    description: "A Gamer Notebook!"
+                    name: value(
+                            test("Notebook X11"),
+                            stub(nonBlank())
+                    ),
+                    brand: value(
+                            test("Deep Driver"),
+                            stub(nonBlank()),
+                    ),
+                    regularPrice: value(
+                            test(1500.00),
+                            stub(number())
+                    ),
+                    salePrice: value(
+                            test(1500.00),
+                            stub(number())
+                    ),
+                    enabled: value(
+                            test(true),
+                            stub(anyBoolean())
+                    ),
+                    categoryId: value(
+                            test("f5ab7a1e-37da-41e1-892b-a1d38275c2f2"),
+                            stub(anyUuid())
+                    ),
+                    description: value(
+                            test("A Gamer Notebook!"),
+                            stub(optional(nonBlank()))
+                    )
             ])
         }
     }
@@ -28,19 +49,19 @@ Contract.make {
             contentType 'application/json'
         }
         body([
-                id: anyUuid(),
-                addedAt: anyIso8601WithOffset(),
-                name: "Notebook X11",
-                brand: "Deep Driver",
-                regularPrice: 1500.00,
-                salePrice: 1500.00,
-                inStock: false,
-                enabled: true,
-                category: [
-                        id: "f5ab7a1e-37da-41e1-892b-a1d38275c2f2",
+                id          : anyUuid(),
+                addedAt     : anyIso8601WithOffset(),
+                name        : fromRequest().body('$.name'),
+                brand       : fromRequest().body('$.brand'),
+                regularPrice: fromRequest().body('$.regularPrice'),
+                salePrice   : fromRequest().body('$.salePrice'),
+                inStock     : false,
+                enabled     : fromRequest().body('$.enabled'),
+                category    : [
+                        id  : fromRequest().body('$.categoryId'),
                         name: "Notebook"
                 ],
-                description: "A Gamer Notebook!"
+                description : fromRequest().body('$.description')
         ])
     }
 }
