5d0ec1c6820dc558fb1e154fbef47d1e468b968f
Antonio Pires <tonan@outlook.com.br>
2026-01-26 08:07:39 -0300

    Error Simulations with Product Mocks

--- src/test/java/com/algaworks/algashop/product/catalog/contract/base/ProductBase.java
+++ src/test/java/com/algaworks/algashop/product/catalog/contract/base/ProductBase.java
@@ -1,5 +1,6 @@
 package com.algaworks.algashop.product.catalog.contract.base;
 
+import com.algaworks.algashop.product.catalog.application.ResourceNotFoundException;
 import com.algaworks.algashop.product.catalog.application.product.management.ProductInput;
 import com.algaworks.algashop.product.catalog.application.product.management.ProductManagementApplicationService;
 import com.algaworks.algashop.product.catalog.application.product.query.PageModel;
@@ -34,6 +35,10 @@ public class ProductBase {
 
     public static final UUID validProductId = UUID.fromString("fffe6ec2-7103-48b3-8e4f-3b58e43fb75a");
 
+    public static final UUID invalidProductId = UUID.fromString("21651a12-b126-4213-ac21-19f66ff4642e");
+
+    public static final UUID createdProductId = UUID.fromString("f7c6843f-465c-476d-9a9b-4783bde4dc5e");
+
     @BeforeEach
     void setUp() {
         RestAssuredMockMvc.mockMvc(MockMvcBuilders.webAppContextSetup(context)
@@ -41,14 +46,23 @@ public class ProductBase {
 
         RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
 
-        mockValidOrderFindById();
+        mockValidProductFindById();
         mockFilterProducts();
         mockCreateProduct();
+        mockInvalidProductFindById();
+    }
+
+    private void mockInvalidProductFindById() {
+        Mockito.when(productQueryService.findById(invalidProductId))
+                .thenThrow(new ResourceNotFoundException());
     }
 
     private void mockCreateProduct() {
         Mockito.when(productManagementApplicationService.create(Mockito.any(ProductInput.class)))
-                .thenReturn(validProductId);
+                .thenReturn(createdProductId);
+
+        Mockito.when(productQueryService.findById(createdProductId))
+                .thenReturn(ProductDetailOutputTestDataBuilder.aProduct().inStock(false).build());
     }
 
     private void mockFilterProducts() {
@@ -71,7 +85,7 @@ public class ProductBase {
                 });
     }
 
-    private void mockValidOrderFindById() {
+    private void mockValidProductFindById() {
         Mockito.when(productQueryService.findById(validProductId))
                 .thenReturn(ProductDetailOutputTestDataBuilder.aProduct().id(validProductId).build());
     }
