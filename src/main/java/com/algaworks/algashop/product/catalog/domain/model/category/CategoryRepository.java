9da1d1837aa01dea926ba495e8f1cef0d501b154
Antonio Pires <tonan@outlook.com.br>
2026-05-19 08:13:03 -0300

    Testing Persistence of a Document

--- src/main/java/com/algaworks/algashop/product/catalog/domain/model/category/CategoryRepository.java
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/category/CategoryRepository.java
@@ -2,5 +2,7 @@ package com.algaworks.algashop.product.catalog.domain.model.category;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CategoryRepository extends MongoRepository<Category, String> {
+import java.util.UUID;
+
+public interface CategoryRepository extends MongoRepository<Category, UUID> {
 }
