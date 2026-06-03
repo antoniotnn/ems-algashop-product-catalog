32bf4e8345a10b854a067cf99094beda0f952c8f
Antonio Pires <tonan@outlook.com.br>
2026-06-03 15:57:30 -0300

    Filters with Or and Regex

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -10,6 +10,7 @@ import com.algaworks.algashop.product.catalog.domain.model.product.Product;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductNotFoundException;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -32,6 +33,9 @@ public class ProductQueryServiceImpl implements ProductQueryService {
     private final Mapper mapper;
 
     private final MongoOperations mongoOperations;
+
+    //private static final String findWordRegex = "(?i)(?<= |^)%s(?= |$)"; //%s é do java
+    private static final String findWordRegex = "(?i)%s"; //%s é do java
 
     @Override
     public ProductDetailOutput findById(UUID productId) {
@@ -134,6 +138,17 @@ public class ProductQueryServiceImpl implements ProductQueryService {
             query.addCriteria(Criteria.where("categoryId").in(
                     (Object[]) filter.getCategoriesId()
             ));
+        }
+
+        if (StringUtils.isNotBlank(filter.getTerm())) {
+            String regexExpression = String.format(findWordRegex, filter.getTerm());
+            query.addCriteria(
+                    new Criteria().orOperator(
+                            Criteria.where("name").regex(regexExpression),
+                            Criteria.where("brand").regex(regexExpression),
+                            Criteria.where("description").regex(regexExpression)
+                    )
+            );
         }
 
 
