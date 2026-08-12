2d44409641e1633e820b0097eeb1bb80afaabf65
Antonio Pires <tonan@outlook.com.br>
2026-08-12 08:53:04 -0300

    Domain Service for stock changes

--- src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/QuantityInStockAdjustment.java
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/QuantityInStockAdjustment.java
@@ -10,5 +10,14 @@ public interface QuantityInStockAdjustment {
             UUID productId,
             int previousQuantity,
             int newQuantity
-    ) {}
+    ) {
+
+        public boolean isOutOfStock() {
+            return newQuantity == 0 && previousQuantity != 0;
+        }
+
+        public boolean isRestocked() {
+            return newQuantity > 0 && previousQuantity == 0;
+        }
+    }
 }
